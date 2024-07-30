ea88e22eba325faf2d98fefa1a997a5eacdeae53
Leandro Cunha <leandrocunha016@gmail.com>
2024-07-29 21:44:36 -0300

    chore: I want button in other color

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -112,7 +112,7 @@
                         Limpar dados
                     </button>
                     <button type="button" @click="isOpen = false"
-                        class="px-6 py-2 text-red-800 border border-red-600 rounded ml-2">
+                        class="px-6 py-2 text-black border border-gray-400 hover:bg-gray-300 rounded ml-2">
                         Cancelar
                     </button>
                 </form>

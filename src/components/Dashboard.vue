9e5bc1bdea31ef8a5381963eaf57a857248d18a5
Leandro Cunha <leandrocunha016@gmail.com>
2024-07-31 15:58:18 -0300

    chore: Change hover bg color for black in dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -59,7 +59,7 @@
         <!-- Button to Open Modal -->
         <div class="mb-6 flex justify-center">
             <button @click="isOpen = true"
-                class="px-6 py-2 text-white bg-blue-600 rounded shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                class="px-6 py-2 text-white bg-blue-600 rounded shadow focus:outline-none focus:ring-2 focus:ring-blue-500 hover:bg-black">
                 Cadastrar tarefa
             </button>
         </div>
